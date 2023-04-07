fd0b9bb26e7b2878128aef8201595a9a0298cba9
Bartosz Kawa <bartekkawa2@gmail.com>
2023-04-07 17:38:25 +0200

    zad 4

--- Lab_123.../Programowanie/src/WordCounter.java
+++ Lab_123.../Programowanie/src/WordCounter.java
@@ -1,47 +1,82 @@
-import javax.swing.*;
+//import javax.swing.*;
 import java.io.*;
-
+import java.util.regex.Pattern;
 
 public class WordCounter {
 
+    private static Pattern pattern = Pattern.compile("\\s{2,}");
+
     public static void main(String[] args) throws IOException{
 
-//        String file_name = JOptionPane.showInputDialog("Podaj nazwe pliku .txt : ");
+        // potrzebne zeby dawac opcje przed nazwa pliku
+        String fileName = "";
+        int lineFlag = 0;
+        int wordFlag = 0;
+        int charFlag = 0;
 
-        if ( args[0] == null || args[0].length() == 0){    // NullPointerException is handled
+        for (String arg : args){
+            if(arg.contains("-") && arg.contains("l")){
+                lineFlag ++;
+            }
+            if(arg.contains("-") && arg.contains("w")){
+                wordFlag ++;
+            }
+            if(arg.contains("-") && arg.contains("c")){
+                charFlag ++;
+            }
+            if(!arg.contains("-")){
+                fileName = arg;
+            }
+        }
+        if (args.length == 0 || fileName.equals("") || (lineFlag == 0 && wordFlag == 0 && charFlag == 0 )){    // NullPointerException is handled
             System.out.println("Brak argumentów programu ");
             return;
         }
-        File f1 = new File(args[0]);
+
+        File f1 = new File(fileName);
+        String line = "";
         int lineCount = 0;
         int charCount = 0;
         int wordCount = 0;
-        try (BufferedReader file = new BufferedReader(new FileReader(args[0]))){
-            while (file.readLine() != null) {
+
+        try (BufferedReader file = new BufferedReader(new FileReader(fileName))){
+            while (true) {
+                line = file.readLine();
+                if (line == null) {
+                    file.close();
+                    break;
+                }
+                charCount += line.replaceAll("\\s", "").length();
+                wordCount += pattern.matcher(line).replaceAll(" ").split(" ").length;
                 lineCount ++;
             }
-            file.close();
 
-            for (String arg : args){
-                if(arg.contains("-") && arg.contains("l")){
-                    JOptionPane.showMessageDialog(null, "Liczba linii w pliku " + args[0] + " wynosi " + lineCount);
-                    System.out.println("Liczba wierszy w pliku abc.txt wynosi: " + lineCount);
-                }
+            if(lineFlag > 0){
+                System.out.println("wierszy : " + lineCount);
             }
-
+            if(wordFlag > 0){
+                System.out.println("znaków : " + wordCount);
+            }
+            if(charFlag > 0){
+                System.out.println("słów : " + charCount);
+            }
         }
         catch (FileNotFoundException exc){
             if(!f1.exists()){
-                System.err.println("Plik " + args[0] + " nie istnieje");
+                System.err.println("Plik " + fileName + " nie istnieje");
+//                JOptionPane.showMessageDialog(null, "Plik  "+ fileName + " nie istnieje");
             }
             else if (f1.isDirectory()) {
-                System.err.println("Plik " + args[0] + " jest katalogiem");
+                System.err.println("Plik " + fileName + " jest katalogiem");
+//                JOptionPane.showMessageDialog(null, "Plik " + fileName + " jest katalogiem");
             }
             else if (!f1.canRead()) {
-                System.err.println("Brak uprawnien do odczytu z pliku " + args[0]);
+                System.err.println("Brak uprawnien do odczytu z pliku " + fileName);
+//                JOptionPane.showMessageDialog(null, "rak uprawnien do odczytu z pliku " + fileName);
             }
             else{
-                System.err.println("Nie znaleziono pliku " + args[0]);
+                System.err.println("Nie znaleziono pliku " + fileName);
+//                JOptionPane.showMessageDialog(null, "Nie znaleziono pliku " + fileName);
             }
         }
         catch (ArrayIndexOutOfBoundsException exc){
