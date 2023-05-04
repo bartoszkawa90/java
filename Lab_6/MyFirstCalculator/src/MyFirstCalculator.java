1293826a9900d004f387e8febb9a8597320d4b22
Bartosz Kawa <bartekkawa2@gmail.com>
2023-05-05 00:19:54 +0200

    L6 zad 1

---
+++ Lab_6/MyFirstCalculator/src/MyFirstCalculator.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class MyFirstCalculator {
+
+    public static void createAndShowGUI() {
+    // Setup
+        JFrame jf = new JFrame("My First Calculator");
+
+    // Pole text i przyciski
+        JTextField line = new JTextField(22);
+        line.setFont(new Font("Courier", Font.BOLD,22));
+        line.setHorizontalAlignment(SwingConstants.RIGHT);
+        line.setText("0");
+
+        JButton ZeroButton = new JButton("0");
+        ZeroButton.setPreferredSize(new Dimension(65,28));
+        JButton OneButton = new JButton("1");
+        OneButton.setPreferredSize(new Dimension(65,28));
+        JButton TwoButton = new JButton("2");
+        TwoButton.setPreferredSize(new Dimension(65,28));
+        JButton ThreeButton = new JButton("3");
+        ThreeButton.setPreferredSize(new Dimension(65,28));
+        JButton FourButton = new JButton("4");
+        FourButton.setPreferredSize(new Dimension(65,28));
+        JButton FiveButton = new JButton("5");
+        FiveButton.setPreferredSize(new Dimension(65,28));
+        JButton SixButton = new JButton("6");
+        SixButton.setPreferredSize(new Dimension(65,28));
+        JButton SevenButton = new JButton("7");
+        SevenButton.setPreferredSize(new Dimension(65,28));
+        JButton EightButton = new JButton("8");
+        EightButton.setPreferredSize(new Dimension(65,28));
+        JButton NineButton = new JButton("9");
+        NineButton.setPreferredSize(new Dimension(65,28));
+        JButton PlusButton = new JButton("+");
+        PlusButton.setPreferredSize(new Dimension(65,28));
+        JButton MinusButton = new JButton("-");
+        MinusButton.setPreferredSize(new Dimension(65,28));
+        JButton MulButton = new JButton("*");
+        MulButton.setPreferredSize(new Dimension(65,28));
+        JButton DivButton = new JButton("/");
+        DivButton.setPreferredSize(new Dimension(65,28));
+        JButton ClearButton = new JButton("C");
+        ClearButton.setPreferredSize(new Dimension(65,28));
+        JButton EqualButton = new JButton("=");
+        EqualButton.setPreferredSize(new Dimension(65,28));
+
+        jf.getContentPane().add(line);
+        jf.getContentPane().add(OneButton);
+        jf.getContentPane().add(TwoButton);
+        jf.getContentPane().add(ThreeButton);
+        jf.getContentPane().add(PlusButton);
+        jf.getContentPane().add(FourButton);
+        jf.getContentPane().add(FiveButton);
+        jf.getContentPane().add(SixButton);
+        jf.getContentPane().add(MinusButton);
+        jf.getContentPane().add(SevenButton);
+        jf.getContentPane().add(EightButton);
+        jf.getContentPane().add(NineButton);
+        jf.getContentPane().add(MulButton);
+        jf.getContentPane().add(ZeroButton);
+        jf.getContentPane().add(EqualButton);
+        jf.getContentPane().add(ClearButton);
+        jf.getContentPane().add(DivButton);
+
+
+        jf.pack();
+        jf.setLayout(new FlowLayout());
+        jf.setLocationRelativeTo(null);
+        jf.setBackground(Color.GRAY);
+        jf.setSize(330,210);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() { createAndShowGUI(); }
+        });
+    }
+
+}
