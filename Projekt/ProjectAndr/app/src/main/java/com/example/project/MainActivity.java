0b0d56c17632034f83b15d60cc47974e57e68ac4
bartoszkawa90 <bartekkawa2017@gmail.com>
2023-06-14 22:00:58 +0200

    Zmienione na TextView z linkami

--- Projekt/ProjectAndr/app/src/main/java/com/example/project/MainActivity.java
+++ Projekt/ProjectAndr/app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -18,25 +27,15 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     Button ExitButton,ReloadButton;
-
-//    public static String fix(String head, String link){
-//        int maxLength = 60;
-//        char[] charHead = head.toCharArray();
-//        if ( head.length() >= maxLength ){
-//            for(int i=0; i< (int) head.length()/maxLength; i++){
-//
-//            }
-//        }
-//    }
+    public static int I;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Exit button action
-        this.ExitButton = (Button)this.findViewById(R.id.ExitButton);
+        this.ExitButton = (Button) this.findViewById(R.id.ExitButton);
         this.ExitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,97 +44,127 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         // Reload button action
-        this.ReloadButton = (Button)this.findViewById(R.id.ReloadButton);
+        this.ReloadButton = (Button) this.findViewById(R.id.ReloadButton);
         this.ReloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String title = WebNews.getTitle();
                 Map<String, String> News = WebNews.getSetOfNews();
 
-                TextView NewsTextView = (TextView)findViewById(R.id.WebsiteTextView);
+                TextView NewsTextView = (TextView) findViewById(R.id.WebsiteTextView);
                 NewsTextView.setText(title);
 
-//                TextView Logs = (TextView)findViewById(R.id.LogsTextView);
-//                Logs.setText(WebNews.numberOfArticles+"");
+
+                TextView articles = (TextView) findViewById(R.id.ArticlesTextView);
+                articles.setText("");
+
+                for ( int i=0; i<WebNews.numberOfArticles; i++){
+                    String head = WebNews.News.entrySet().toArray()[I].toString().split("=")[0];
+                    String link = WebNews.News.entrySet().toArray()[I].toString().split("=")[1];
+                    I = i;
+
+                    SpannableString spannableString = new SpannableString(
+                            (I+1) + " : " + head
+                    );
+
+                    ClickableSpan span1 = new ClickableSpan() {
+                        @Override
+                        public void onClick(@NonNull View view) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse(link));
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void updateDrawState(@NonNull TextPaint ds) {
+                            ds.setColor(Color.BLUE);
+                            ds.setUnderlineText(false);
+                        }
+                    };
+
+                    spannableString.setSpan(span1, 1, head.length() + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    articles.append(spannableString);
+                    articles.append("\n\n");
+                    articles.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+                }
             }
         });
 
-        // Start Thread for collecting news
+        // Zacznij zbierac informacje i zaczekajz głównym wątkiem
         Thread CollectingNewsThread = new Thread(new WebNews());
         CollectingNewsThread.start();
-
-        // Thread main starts 10s after collecting news
         try {
             TimeUnit.SECONDS.sleep(3);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
 
-        // Table
-        TableLayout stk = (TableLayout) findViewById(R.id.tableLayout);
-        TableRow tbrow0 = new TableRow(this);
-        TextView tv0 = new TextView(this);
-        tv0.setText("               Article Title ");
-        tv0.setTextColor(Color.BLACK);
-        tv0.setTextSize(18);
-        tbrow0.addView(tv0);
 
-        TextView tv1 = new TextView(this);
-        tv1.setText("                   Link ");
-        tv1.setTextColor(Color.BLACK);
-        tv1.setTextSize(18);
-        tbrow0.addView(tv1);
-        stk.addView(tbrow0);
+        TextView articles = (TextView) findViewById(R.id.ArticlesTextView);
 
-        for (int i = 0; i < WebNews.numberOfArticles; i++) {
-            String head = Integer.toString(i+1) + " : " + WebNews.News.entrySet().toArray()[i].toString().split("=")[0];
-            String link = Integer.toString(i+1) + " : " + WebNews.News.entrySet().toArray()[i].toString().split("=")[1];
+        for ( int i=0; i<WebNews.numberOfArticles; i++){
+            String head = WebNews.News.entrySet().toArray()[I].toString().split("=")[0];
+            String link = WebNews.News.entrySet().toArray()[I].toString().split("=")[1];
+            I = i;
 
-            TextView Logs = (TextView)findViewById(R.id.LogsTextView);
-            if(i == WebNews.numberOfArticles-1) { Logs.setText(head); }
+            SpannableString spannableString = new SpannableString(
+                    (I+1) + " : " + head
+            );
 
-            TableRow tbrow = new TableRow(this);
-            TextView headline1 = new TextView(this);
-            headline1.setText(head);
-            headline1.setTextColor(Color.BLACK);
-            headline1.setTextSize(14);
-            tbrow.addView(headline1);
+            ClickableSpan span1 = new ClickableSpan() {
+                @Override
+                public void onClick(@NonNull View view) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(link));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
 
-//            TextView link1 = new TextView(this);
-//            link1.setText(link);
-//            link1.setTextColor(Color.BLACK);
-//            link1.setTextSize(14);
-//            tbrow.addView(link1);
-            stk.addView(tbrow);
+                @Override
+                public void updateDrawState(@NonNull TextPaint ds) {
+                    ds.setColor(Color.BLUE);
+                    ds.setUnderlineText(false);
+                }
+            };
+
+            spannableString.setSpan(span1, 1, head.length() + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            articles.append(spannableString);
+            articles.append("\n\n");
+            articles.setMovementMethod(LinkMovementMethod.getInstance());
+            if (I == WebNews.numberOfArticles-1) { I = 0; }
+
         }
-    }
 
 
-//            TableRow tbrow = new TableRow(this);
-//            TextView t1v = new TextView(this);
-//            t1v.setText("" + i);
-//            t1v.setTextColor(Color.BLACK);
-//            t1v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t1v);
+
+//        TextView articles = (TextView) findViewById(R.id.ArticlesTextView);
 //
-//            TextView t2v = new TextView(this);
-//            t2v.setText("Product " + i);
-//            t2v.setTextColor(Color.BLACK);
-//            t2v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t2v);
-//            TextView t3v = new TextView(this);
-//            t3v.setText("Rs." + i);
-//            t3v.setTextColor(Color.BLACK);
-//            t3v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t3v);
-//            TextView t4v = new TextView(this);
-//            t4v.setText("" + i * 15 / 32 * 10);
-//            t4v.setTextColor(Color.BLACK);
-//            t4v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t4v);
-//            stk.addView(tbrow);
-//        }
+//        SpannableString spannableString = new SpannableString(
+//                WebNews.News.entrySet().toArray()[0].toString().split("=")[0]
+//        );
+//
+//        ClickableSpan span1 = new ClickableSpan() {
+//            @Override
+//            public void onClick(@NonNull View view) {
+//                Intent intent = new Intent(Intent.ACTION_VIEW,
+//                        Uri.parse(WebNews.News.entrySet().toArray()[0].toString().split("=")[1]));
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(intent);
+//            }
+//
+//            @Override
+//            public void updateDrawState(@NonNull TextPaint ds) {
+//                ds.setColor(Color.BLUE);
+//                ds.setUnderlineText(false);
+//            }
+//        };
+//
+//        spannableString.setSpan(span1, 9, 15, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        articles.setText(spannableString);
+//        articles.setMovementMethod(LinkMovementMethod.getInstance());
 
-
+    }
 }
